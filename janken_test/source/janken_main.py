6902d6a3e67887dce1ba4c66aeaa29f99ca18531
mukaiguti <y.mukaiguchi.sys24@morijyobi.ac.jp>
2024-12-06 17:55:17 +0900

    janken_mainを更新

--- janken_test/source/janken_main.py
+++ janken_test/source/janken_main.py
@@ -1,44 +1,51 @@
-import player
-import computer
-import janken_judge
+from source import player
+from source import computer
+from source import janken_judge
+
+
+def play_round(round_number, player_win, computer_win):
+    """1ラウンドのじゃんけんを行う処理"""
+    print(f"----- ラウンド {round_number} -----")
+    computer_hand = computer.computer_pon()
+    player_hand = player.player_pon()
+    result = janken_judge.judge(computer_hand, player_hand)
+
+    print(f"あなたの手: {player_hand}")
+    print(f"コンピューターの手: {computer_hand}\n")
+
+    if result == 'draw':
+        print("あいこです！再度対決！\n")
+        return player_win, computer_win, False  # ラウンドを繰り返す
+    else:
+        if result == 'player_win':
+            player_win += 1
+            print("あなたの勝ちです！\n")
+        else:
+            computer_win += 1
+            print("コンピューターの勝ちです！\n")
+        return player_win, computer_win, True  # ラウンドを進める
+
+def print_final_result(player_win, computer_win):
+    """最終結果を表示する処理"""
+    print("【最終結果】")
+    print(f"あなた: {player_win}勝")
+    print(f"コンピュータ: {computer_win}勝")
+    if player_win >= computer_win:
+        print("あなたの総合勝利です！")
+    else:
+        print("コンピュータの総合勝利です！")
 
 def main():
-   player_win = 0
-   computer_win = 0
+    player_win = 0
+    computer_win = 0
+    round_number = 1
 
-   """3回戦のじゃんけんゲームを行う関数"""
-   # hands = ['グー', 'チョキ', 'パー']
+    while round_number <= 3:
+        player_win, computer_win, proceed = play_round(round_number, player_win, computer_win)
+        if proceed:
+            round_number += 1
 
-   round = 1
-   while round <= 3:
-       print(f"-----ラウンド {round} -----")
-       computer_hand = computer.computer_pon()
-       player_hand = player.player_pon()
-       result = janken_judge.judge(computer_hand,player_hand)
-
-       print(f"あなたの手:{player_hand}")
-       print(f"コンピューターンの手:{player_hand}")
-
-       print("")  
-       if result == 'draw':
-         print("あいこです！ 再度対決！")    
-       else:
-         round += 1 
-         if result == 'player_win':
-             player_win +=1
-             print("あなたの勝ちです！")
-         else:
-             computer_win +=1
-             print("コンピューターの勝ちです！")            
-       print("")
-
-   print("【最終結果】")
-   print(f"あなた:{player_win}勝")
-   print(f"コンピュータ:{computer_win}勝")
-   if player_win >= computer_win:
-     print("あなたの総合勝利です！")
-   else:
-      print("コンピュータの総合勝利です！")
+    print_final_result(player_win, computer_win)
 
 if __name__ == '__main__':
-   main()
+    main()
